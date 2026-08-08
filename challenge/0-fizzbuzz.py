a569006bd6ef1d5d1872f5d17af6ae79178e292b
Izzat <shixbalayevizzet@example.com>
2026-08-08 18:50:24 +0000

    task0

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, 100):
+for i in range(1, int(sys.argv[1]) + 1):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz", end=" ")
     elif i % 3 == 0:
