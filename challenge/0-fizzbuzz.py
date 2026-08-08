5515339837f5c83c380909ba4ccf68b50c13d673
Izzat <shixbalayevizzet@example.com>
2026-08-08 18:52:24 +0000

    task0

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python3
+"""
+FizzBuzz implementation
+"""
 import sys
 
-for i in range(1, int(sys.argv[1]) + 1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end=" ")
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    else:
-        print(i, end=" ")
+
+def fizzbuzz(n):
+    """
+    Prints numbers from 1 to n separated by a space.
+    - Multiples of 3 print "Fizz"
+    - Multiples of 5 print "Buzz"
+    - Multiples of both 3 and 5 print "FizzBuzz"
+    """
+    if n < 1:
+        return
+
+    result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            result.append("Fizz")
+        elif (i % 5) == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    print(" ".join(result))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
