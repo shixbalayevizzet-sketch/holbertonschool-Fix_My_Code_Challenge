e303f24885390f7ffe1592356aa81f8217771e18
Izzat <shixbalayevizzet@example.com>
2026-08-08 19:26:09 +0000

    task4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,48 +1,56 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list.
- * @head: Pointer to the head of the list
- * @index: Index of the node to delete
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = *head;
-	unsigned int i = 0;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
 	if (head == NULL || *head == NULL)
+	{
 		return (-1);
-
-	/* Əgər silinəcək element başlanğıcdırsa (index 0) */
-	if (index == 0)
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
 	{
 		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
+		p++;
 	}
-
-	/* Lazımi indeksə qədər irəliləyirik */
-	while (current != NULL && i < index)
+	if (p != index || *head == NULL)
 	{
-		current = current->next;
-		i++;
-	}
-
-	/* Əgər indeks siyahının uzunluğundan böyükdürsə */
-	if (current == NULL)
+		*head = saved_head;
 		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		dlistint_t *current = *head;
 
-	/* Qonşu nodların göstəricilərini yeniləyirik */
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-
-	if (current->prev != NULL)
-		current->prev->next = current->next;
-
-	free(current);
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+		if (current->prev != NULL)
+			current->prev->next = current->next;
+		free(current);
+		*head = saved_head;
+	}
 	return (1);
 }
